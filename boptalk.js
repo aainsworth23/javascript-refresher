55f7c8f71107de33c73bae433d6bcad197daa62e
aainsworth23 <aainsworth23@penguinhall.org>
2022-04-05 10:08:55 -0400

    boptalk

--- boptalk.js
+++ boptalk.js
@@ -1,7 +1,16 @@
-let words = 'tuna is good';
-console.log(words);
-
+let inputMessage = 'tuna is good'
 
 let text = ['a', 'e', 'i', 'o', 'u'];
-const myArray = text.split(",");
+let letter = 'b';
+
+if (text.includes(letter) > 1) {
+    console.log('text');
+} else {
+    console.log('consonant');
+}
+
+let 
+
+
+
 
