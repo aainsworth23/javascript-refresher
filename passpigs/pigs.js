0dbf0742018c519e66efea97adc8e3ac3cc921e5
aainsworth23 <aainsworth23@penguinhall.org>
2022-04-15 10:15:53 -0400

    class work

--- passpigs/pigs.js
+++ passpigs/pigs.js
@@ -1,19 +1,30 @@
+let playerTurn = 0;
+
 function handleClick(id) {
     console.log(id);
 
     if (id == 'player0RollButton') {
         console.log('Roll');
         //  ??
-    } else { 
+    } else {
         handlePass();
 
-        
+
+    }
+    //if(document.getElementById('player0RollButton'
+
 }
-//if(document.getElementById('player0RollButton'
 
 function handlePass() {
-    console.log('Pass');
+    console.log('Pass player' + playerTurn);
 
-let myElement = document.getElementById('player0');
-myElement.setAttribute('w3-card w3-container w3-dark-gray w3-round-large', 'w3-card w3-container w3-gray w3-round-large')
-} 
+   // document.getElementById() = 'player' + playerTurn
+
+    let playersdiv = document.getElementById('player' + playerTurn);
+    playersdiv.setAttribute('class', 'w3-card w3-container w3-light-gray w3-round-large')
+
+    let playerdiv = document.getElementById('player' + (playerTurn + 1));
+    playerdiv.setAttribute('class', 'w3-card w3-container w3-dark-gray w3-round-large')
+
+    playerTurn = playerTurn + 1
+}
